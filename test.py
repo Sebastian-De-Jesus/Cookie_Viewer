5f6dbeebee3dfdcf86e6945f2e8adae763cce2b3
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2023-10-01 15:37:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ import sqlite3
 import glob
 import os
 import csv
+from os.path import *
+
+
+
+def clear_file(fileName = None):
+    #Checks if results exisits deletes its contents if it does.
+    if exists("result.txt"):
+        with open("result.txt", "w") as file:
+            file.truncate(0)
+
+
 
 #Finding the users correct USER name that will be needed in order to find that users file path for cookies
 x = os.environ['USERPROFILE']
@@ -44,11 +55,30 @@ for cookie in cookies:
         i += 1
 
 
+clear_file(fileName="result.txt")
+
 #This will print out your print out your cookie values aswell as thier uses to your terminal
 #additionally this will also write the found values to a .txt file for use later
 for key in result:
-    print(f"This is the key : '{key}' and this is the value : '{result[key]}")
+    #Line below prints out to the terminal the cookie:function
+    #print(f"This is the key : '{key}' and this is the value : '{result[key]}") 
+           
+    #writes new contents to results
     with open("result.txt","a") as file:
         file.write(f"{str(key)} : {str(result[key])}\n")
 
+#Attempting to remove the "[, ] characters from our file and writing back the altered lines"
+symbol_remove = ['[',']',"'",'"']
+with open("result.txt","r") as file: 
+    lines = file.readlines()
+ 
+#Next three lines allows us to remove all unwanted charaters from out cookie:function txt
+for i in range(len(lines)):
+    for symbol in symbol_remove:
+        lines[i] = lines[i].replace(symbol, '')
+
+#writes the changes back to our result.txt for cleaner output.
+with open("result.txt", 'w') as file:
+    file.writelines(lines)
+    
 conn.close()
